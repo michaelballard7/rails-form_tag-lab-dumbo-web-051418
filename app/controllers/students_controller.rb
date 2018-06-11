f2c3397477e713f58faf7d1025d1074c7e9210a1
Michael Anthony Ballard <MichaelBallard@Michaels-MacBook-Pro.local>
2018-06-11 08:33:00 -0400

    Completed Lab

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
@@ -7,5 +7,27 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
+
+  def new
+
+    render :new
+  end
+
+  def create
+    Student.create(student_params)
+
+    redirect_to students_path
+  end
+
+
+
+  private
+  # I can get access to mass assignment with the below method
+  # I call this param unpacking
+  def student_params
+    params.permit(:first_name, :last_name)
+  end
+
+
 
 end
